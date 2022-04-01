6fa11dfc634792f722fa3d63cbe032b67a444a4d
darkoje <fusion.performer@gmail.com>
2022-04-01 23:46:54 +0200

    major popup UI update

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 /* jshint esversion: 6 */
 /* globals chrome */
 
@@ -17,9 +16,7 @@ function encipher(str){
           var yoda = str.charCodeAt(i);
           if (yoda >= 65 && yoda <= 90) {c = String.fromCharCode(((yoda - 65 + how_much) % 26) + 65);}
           else if (yoda >= 97 && yoda <= 122) {c = String.fromCharCode(((yoda - 97 + how_much) % 26) + 97);}
-        }
-        result += c;
-      }
+        } result += c; }
       return result;
     };
     let one = encode(str);
@@ -40,21 +37,18 @@ function generateActivationCode(length){
     let lettersLength = letters.length;
     for ( let i = 0; i < length; i++ ) {
       activation_code += letters.charAt(Math.floor(Math.random() * lettersLength));
-    }
-    return activation_code;
+    } return activation_code;
 }
 
 // CHECK SERIAL API
 function checkSerial(serial){
     let enc_serial = encipher(serial);
     let endpoint = API_SERVER + "checkserial.php?monty=" + enc_serial;
+
     fetch(endpoint)
-    .then((response) => {
-        return response.text();
-    })
+    .then((response) => {return response.text();})
     .then((data) => {
         if (data=="ready"){
-            // tbd: ask user whether to activate
             document.getElementById("message").textContent = "serial ready, licensing...";
             document.getElementById("keyStatus").textContent = "ready";
             activateLicense(serial);
@@ -64,20 +58,22 @@ function checkSerial(serial){
             checkLicense(serial);
         } else if (data=="expired"){
             document.getElementById("message").textContent = "license expired :(";
+            document.getElementById("message").style.color = "indianred";
             document.getElementById("keyStatus").textContent = "expired";
             document.getElementById("inputForm").style.display = "table";
             document.getElementById("activationStatus").textContent="expired";
             saveError("errOOr");
         } else if (!data){
             document.getElementById("message").textContent = "wrong serial number";
-            document.getElementById("message").style.color = "red";
+            document.getElementById("message").style.color = "indianred";
             document.getElementById("keyStatus").textContent = "wrong";
             document.getElementById("inputForm").style.display = "table";
+            document.getElementById("submittedSerial").textContent="wrong";
             document.getElementById("activationStatus").textContent="not licensed";
-            document.getElementById("atoggle").textContent="* wrong serial number";
             saveError("errOOr");
         } else {
             document.getElementById("message").textContent = "that's weird";
+            document.getElementById("message").style.color = "indianred";
             document.getElementById("keyStatus").textContent = "weird";
             document.getElementById("inputForm").style.display = "table";
             saveError("errOOr");
@@ -87,28 +83,19 @@ function checkSerial(serial){
         document.getElementById("message").textContent = "problem connecting to api server";
         document.getElementById("inputForm").style.display = "table";
     });
-
 }
 
 // ACTIVATE LICENSE API
 function activateLicense(serial){
-
     chrome.storage.sync.get(["hotm-license"], function(result) {
         let activation_code = result["hotm-license"];
         let license_key;
-        if (activation_code==null){
-            license_key = generateActivationCode(14);
-        } else {
-            license_key = activation_code;
-        }
-        let enc_serial = encipher(serial);
-        let enc_license = encipher(license_key);
-
-        saveLicenseKey(license_key);
-        saveSerial(serial);
+        if (activation_code==null){license_key = generateActivationCode(14);} else {license_key = activation_code;}
+        let enc_serial = encipher(serial); let enc_license = encipher(license_key);
+        saveLicenseKey(license_key);saveSerial(serial);
         chrome.storage.sync.set({"hotm-error": "err00r"}, function() {});
-
         let endpoint = API_SERVER + "activate.php?monty=" + enc_serial + "&python=" + enc_license;
+
         fetch(endpoint).then((response) => {return response.text();}).then((data) => {
             document.getElementById("message").textContent = "checking license...";
             checkLicense(serial);
@@ -125,8 +112,11 @@ function checkLicense(serial){
         let activation_code = result["hotm-license"];
         if (activation_code==null){
             document.getElementById("message").textContent = "activated by another user :(";
+            document.getElementById("message").style.color = "indianred";
             document.getElementById("activationStatus").textContent = "no license";
             document.getElementById("inputForm").style.display = "table";
+            document.getElementById("allGood").style.display = "none";
+            document.getElementById("notGood").style.display = "block";
         } else {
 
             let enc_serial = encipher(serial);
@@ -139,25 +129,25 @@ function checkLicense(serial){
                     document.getElementById("message").style.color = "seagreen";
                     document.getElementById("inputForm").style.display = "none";
                     document.getElementById("activationStatus").textContent = data;
-                    document.getElementById("atoggle").style.display = "table";
-                    document.getElementById("atoggle").textContent = "*";
-                    document.getElementById("atoggle").style.color = "black";
+                    document.getElementById("message").style.color = "seagreen";
+                    document.getElementById("allGood").style.display = "block";
+                    document.getElementById("notGood").style.display = "none";
                     chrome.storage.sync.set({"hotm-error": "err00r"}, function() {});
                } else if (data==""){
-                    document.getElementById("message").textContent = "used elsewhere";
-                    document.getElementById("message").style.color = "red";
                     document.getElementById("activationStatus").textContent = "not licensed";
                     document.getElementById("inputForm").style.display = "table";
-                    document.getElementById("atoggle").style.display = "table";
-                    document.getElementById("atoggle").textContent = "* activated by another instance";
-                    document.getElementById("atoggle").style.color = "red";
-
+                    document.getElementById("message").textContent = "* activated on another instance";
+                    document.getElementById("message").style.color = "indianred";
+                    document.getElementById("allGood").style.display = "none";
+                    document.getElementById("notGood").style.display = "block";
                     chrome.storage.sync.set({"hotm-error": "errOOr"}, function() {});
                }
             }).catch((error) => {
                 document.getElementById("message").textContent = "problem connecting to api server";
                 document.getElementById("activationStatus").textContent = "error";
                 document.getElementById("inputForm").style.display = "table";
+                document.getElementById("allGood").style.display = "none";
+                document.getElementById("notGood").style.display = "block";
             });
         }
     });
@@ -171,26 +161,6 @@ submitSerialButton.addEventListener("click", () => {
   checkSerial(registrationKey);
 });
 
-// TOGGLE CLICK LISTENER
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('atoggle');
-    link.addEventListener('click', function() {
-        let notice = document.getElementById('atoggle');
-        if (notice.innerText == "*") {
-          notice.style.display = "block";
-          document.getElementById("inputForm").style.display = "table";
-          document.getElementById("atoggle").textContent = "* Warning, one user install per serial allowed!";
-          document.getElementById("atoggle").style.fontWeight = "450";
-          document.getElementById("atoggle").style.color = "red";
-        } else {
-            document.getElementById("inputForm").style.display = "none";
-            document.getElementById("atoggle").textContent = "*";
-            document.getElementById("atoggle").style.color = "black";
-        }
-        document.getElementById("regkey").placeholder = "input another serial*";
-    });
-});
-
 // CHECK LOCALLY FOR SERIAL
 chrome.storage.sync.get(["hotm-serial"], function(result) {
     let value = result["hotm-serial"];
@@ -201,4 +171,18 @@ chrome.storage.sync.get(["hotm-serial"], function(result) {
         document.getElementById("submittedSerial").textContent = value;
         checkSerial(value);
     }
+});
+
+// LOGO CLICK LISTENER
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('logo');
+    link.addEventListener('click', function() {
+        let inputForm_display = document.getElementById("inputForm").style.display
+        if (inputForm_display == "none") {
+            document.getElementById("inputForm").style.display = "table";
+        } else {
+            document.getElementById("inputForm").style.display = "none";
+        }
+        document.getElementById("regkey").placeholder = "input another serial*";
+    });
 });
